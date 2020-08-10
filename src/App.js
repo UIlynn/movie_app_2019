a085bf4940f04d37feee72bff4a0bebd05683812
spencer <soorte214@gmail.com>
2020-08-10 11:37:56 +0900

    #3 3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import React from 'react';
 
 class App extends React.Component{
-  // Mounting - constructor()
-  constructor(props){
-    super(props)
-    console.log("> constructor") // 1번
-  }
-
   state = {
-    count : 0,
-  };
+    isLoading : true,
+    movies : [],
+  }
 
-  add = () => { 
-    this.setState(current => ({count : current.count +1 }));
-  };
-  minus = () => {
-    this.setState(current => ({count : current.count -1 }));
-  };
-
-  // Mounting - componentDidMout()
+  // render후 첫번째 호출
   componentDidMount(){
-    console.log("> rendered") // 3번
+    setTimeout(()=>{
+      this.setState({isLoading:false})
+    }, 5000);
   }
 
-  // Update - componentDidUpdate()
-  componentDidUpdate(){
-    console.log("> updated") // 4번... 이후 이벤트나 컴포넌트 변경이 있을 경우 render->updated
-  }
-
-  // UnMounting - componentWillUnmount()
-  componentWillUnmount(){
-    console.log("> unmounting~")
-  }
-
+  // render
   render(){
-    console.log("> render") // 2번
+    const {isLoading} = this.state;
+
     return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
-      </div>
+    <div>{isLoading ? "Loading...":"We are ready"}</div>
     )
   }
 }
