2b5a459c978acf907ccf843d67f1639f409074d3
spencer <soorte214@gmail.com>
2020-08-10 19:18:31 +0900

    #6.1 Building the Router (08:51)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home"
+import About from "./routes/About"
 
 function App(){
-  return <span>somthing</span>
+  // url 라우터 설정
+  return (
+    <HashRouter>
+      {/* 두 페이지 동시 렌더링 문제가 있음 */}
+      {/* url 경로가 겹치면(매칭되면) 안 됨. 2개 이상의 라우터가 매칭됨 */}
+      {/* <Route path="/" component={Home} /> */}
+      {/* <Route path="/about" component={About} /> */}
+
+      {/* exact={true} : 완벽할 때 렌더링*/}
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      
+
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function About(){
+    return <span>About this page</span>
+}
+
+export default About;
